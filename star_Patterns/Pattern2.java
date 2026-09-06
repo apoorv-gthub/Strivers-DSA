bcca58dc5b2104fee0c0a4deb3ab8a9fe3b1ab01
apoorv-gthub <aal.apoorv@gmail.com>
2026-09-06 21:04:00 +0530

    Pattern2

--- star_Patterns/Pattern2.java
+++ star_Patterns/Pattern2.java
@@ -1,20 +1,22 @@
 package star_Patterns;
 
-public class Pattern1 {
-	
+public class Pattern2 {
+
 	public static void show() {
 		
-		int n1 = 5;
+		int n2 = 5;
 		
-		for(int i = 0; i < n1; i++) {
-			for(int j = 0; j < n1; j++) {
+		for(int i = 0; i < n2; i++) {
+			for(int j = 0; j <= i; j++) {
 				System.out.print("* ");
 			}System.out.println();
 		}
 	}
 	
+	
 	public static void main(String[] args) {
 		show();
+		
 		
 	}
 
