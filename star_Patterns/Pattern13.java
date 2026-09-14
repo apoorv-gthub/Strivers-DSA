8708fe897a4d7b10b3ee7beee98cce6f81e28d95
apoorv-gthub <aal.apoorv@gmail.com>
2026-09-14 10:38:22 +0530

    Pattern 13

---
+++ star_Patterns/Pattern13.java
@@ -0,0 +1,19 @@
+package star_Patterns;
+
+public class Pattern13 {
+	public static void show(int n) {
+		int count = 1;
+		for (int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(count + " ");
+				count++;
+			}System.out.println();
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		show(5);
+	}
+
+}
