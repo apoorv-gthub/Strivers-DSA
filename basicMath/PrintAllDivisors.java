ccdaddf3e20c92c6ba363e1fa3e945cabd7e25f6
apoorv-gthub <aal.apoorv@gmail.com>
2026-09-19 11:33:00 +0530

    Sort using ArrayList

--- basicMath/PrintAllDivisors.java
+++ basicMath/PrintAllDivisors.java
@@ -1,20 +1,25 @@
 package basicMath;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class PrintAllDivisors {
 
 	public static void main(String[] args) {
 
 		int n = 36;
+		ArrayList<Integer> arr = new ArrayList<>();
 
 		for (int i = 1; i <= Math.sqrt(n); i++) {
 			if (n % i == 0) {
-				System.out.println(i);
+				arr.add(i);
 
 				if ((n / i) != i) {
-					System.out.println(n / i);
+					arr.add(n/i);
 				}
 			}
-
 		}
+		Collections.sort(arr);
+		System.out.println(arr);
 	}
 }
